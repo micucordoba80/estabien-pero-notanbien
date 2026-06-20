37c99bea8f3b9489cb2b8471f86f6ef8675130db
micaela <micu.cordoba80@gmail.com>
2026-06-20 18:43:19 -0300

    fix: centrar el canvas del juego con game-container

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ const config = {
     type: Phaser.AUTO,
     width: GAME.W,
     height: GAME.H,
-    parent: document.body,
+    parent: 'game-container',
     backgroundColor: '#1a1a2e',
     physics: { default: 'arcade', arcade: { gravity: { y: 0 }, debug: false } },
     scale: { mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH },
